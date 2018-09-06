c62863e38c77d36288bc3817950eab1bd33bb3ad
Christabel Escarez and Jentry Jibben <jentry@>
2018-09-05 20:22:32 -0700

    write js to hide and show walrus image and associated text

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
+// $(document).ready(function() {
+//   $("#wally").click(function() {
+//     $(".walrus-showing").show();
+//     $(".walrus-hidden").hide();
+//   });
+// });
+
+// $(document).ready(function() {
+//   console.log("I'm ready.");
+//   $("#wally").click(function() {
+//     console.log("CLICK!!!");
+//     $(".walrus-showing").slideToggle();
+//   });
+//
+// });
+
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
+  console.log("I'm ready.");
+  $(".walrus-hidden").click(function() {
+    console.log("You found me!.");
+    $(".walrus-showing").slideToggle();
+    $(".walrus-hidden").slideToggle();
   });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
+  $(".walrus-showing").click(function() {
+    console.log("Where am I!?!");
+    $(".walrus-showing").slideToggle();
+    $(".walrus-hidden").slideToggle();
   });
 });
