c2d74f5435083a517855963def34f9c98e79b9bc
Christabel Escarez <christabel.escarez@gmail.com>
2018-09-09 15:36:28 -0700

    limit walrus show/hide to 'Click here'

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,32 @@
-// $(document).ready(function() {
-//   $("#wally").click(function() {
-//     $(".walrus-showing").show();
-//     $(".walrus-hidden").hide();
-//   });
-// });
 
 // $(document).ready(function() {
 //   console.log("I'm ready.");
-//   $("#wally").click(function() {
-//     console.log("CLICK!!!");
-//     $(".walrus-showing").slideToggle();
+//   $("#walrus-hidden").click(function() {
+//     console.log("You found me!");
+//     $("#walrus-showing").slideToggle();
+//     $("#walrus-hidden").slideToggle();
 //   });
 //
+//   $("#walrus-showing").click(function() {
+//     console.log("Where did I go?");
+//       $("#walrus-showing").slideToggle();
+//       $("#walrus-hidden").slideToggle();
+//   });
 // });
+
+
 
 $(document).ready(function() {
   console.log("I'm ready.");
-  $(".walrus-hidden").click(function() {
+  $(".clickable").click(function() {
     console.log("You found me!");
-    $(".walrus-showing").slideToggle();
-    $(".walrus-hidden").slideToggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
   });
 
-  $(".walrus-showing").click(function() {
-    console.log("Where did I go?");
-      $(".walrus-showing").slideToggle();
-      $(".walrus-hidden").slideToggle();
-  });
+  // $("#walrus-showing").click(function() {
+  //   console.log("Where did I go?");
+  //     $("#walrus-showing").slideToggle();
+  //     $("#walrus-hidden").slideToggle();
+  // });
 });
-
-
-  // $(document).ready(function() {
-  //   console.log("I'm ready.");
-  //   $(".clickable1").click(function() {
-  //     console.log("You found me!");
-  //     $(".walrus-showing").slideToggle();
-  //     $(".walrus-hidden").slideToggle();
-  // });
-  //
-  //   $(".clickable2").click(function() {
-  //     console.log("Where did I go?");
-  //     $(".walrus-showing").slideToggle();
-  //     $(".walrus-hidden").slideToggle();
-  // });
